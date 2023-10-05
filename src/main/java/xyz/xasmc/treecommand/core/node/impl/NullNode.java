f20758e3e056b271457839bec87ccaeb6111dff2
XiYang6666 <1782356858@qq.com>
2023-10-05 10:56:36 +0800

    :sparkles: support `NullNode`

---
+++ src/main/java/xyz/xasmc/treecommand/core/node/impl/NullNode.java
@@ -0,0 +1,6 @@
+package xyz.xasmc.treecommand.core.node.impl;
+
+import xyz.xasmc.treecommand.core.node.BaseNode;
+
+public class NullNode extends BaseNode {
+}
--- src/main/java/xyz/xasmc/treecommand/core/node/marker/Executable.java
+++ src/main/java/xyz/xasmc/treecommand/core/node/marker/Executable.java
@@ -1,4 +1,9 @@
 package xyz.xasmc.treecommand.core.node.marker;
 
+import xyz.xasmc.treecommand.core.middleware.functional.Middleware;
+
 public interface Executable {
+    Middleware getMiddleware();
+
+    void setMiddleware(Middleware middleware);
 }
